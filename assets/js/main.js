6578fba15808b98547dbce3ee51fcf0af1c07cfc
lamons <lamons.n@gmail.com>
2017-10-25 11:26:32 +0800

    add level 3 entries, modified .gitignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$(".publication-toggle").click(function(){
-		$(".music").hide();
-		$(".about").hide();
+		$(".menu-lv2").hide();
+		$(".lv3").hide();
+		$(".publication-index").css("text-decoration", "none");
 		$(".publication").show("slide", { direction: "left" }, 300);
 		$(".music-toggle").css("text-decoration", "none");
 		$(".about-toggle").css("text-decoration", "none");
@@ -10,8 +11,9 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
 	$(".music-toggle").click(function(){
-		$(".publication").hide();
-		$(".about").hide();
+		$(".menu-lv2").hide();
+		$(".lv3").hide();
+		$(".publication-index").css("text-decoration", "none");
 		$(".music").show("slide", { direction: "left" }, 300);
 		$(".about-toggle").css("text-decoration", "none");
 		$(".publication-toggle").css("text-decoration", "none");
@@ -20,11 +22,22 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
 	$(".about-toggle").click(function(){
-		$(".music").hide();
-		$(".publication").hide();
+		$(".menu-lv2").hide();
+		$(".lv3").hide();
+		$(".publication-index").css("text-decoration", "none");
 		$(".about").show("slide", { direction: "left" }, 300);
 		$(".publication-toggle").css("text-decoration", "none");
 		$(".music-toggle").css("text-decoration", "none");
 		$(this).css("text-decoration", "line-through");
 	});
 });
+
+$(document).ready(function(){
+	$(".joyride").click(function(){
+		$(".lv3").hide();
+		$(".joyride-entry").show("slide", { direction: "left" }, 300);
+		$(".publication-index").css("text-decoration", "none");
+		$(this).css("text-decoration", "line-through");
+	});
+});
+
