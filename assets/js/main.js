6b8b113b3bfdb8b167523a1790a87fb2a6857cbc
lamons <lamons.n@gmail.com>
2017-11-20 23:34:37 +0800

    minified js libs

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		$(".logo3d-container").css({"position": "absolute", "z-index": "-100"});
 		$(".logo3d-container").offset({ top: l3y, left: l3x });
 		$(".pub-content").offset({ top: pty});
-		$(".publication").show("slide", { direction: "left" });
+		$(".publication").show();
 		$(".music-toggle").css("text-decoration", "none");
 		$(".about-toggle").css("text-decoration", "none");
 		$(".publication-index").css("text-decoration", "none");
@@ -20,7 +20,7 @@ $(document).ready(function(){
 		$(".logo3d-container").offset({ top: l3y, left: l3x });
 		$(".music-content").offset({ top: mty});
 		$(".publication-index").css("text-decoration", "none");
-		$(".music").show("slide", { direction: "left" });
+		$(".music").show();
 		$(".about-toggle").css("text-decoration", "none");
 		$(".publication-toggle").css("text-decoration", "none");
 		$(".publication-index").css("text-decoration", "none");
@@ -35,7 +35,7 @@ $(document).ready(function(){
 		$(".logo3d-container").offset({ top: l3y, left: l3x });
 		$(".about-content").offset({ top: aty});
 		$(".publication-index").css("text-decoration", "none");
-		$(".about").show("slide", { direction: "left" });
+		$(".about").show();
 		$(".publication-toggle").css("text-decoration", "none");
 		$(".music-toggle").css("text-decoration", "none");
 		$(".publication-index").css("text-decoration", "none");
