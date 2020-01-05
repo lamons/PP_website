6da1df59c520b0964984e4372080e9c1bf8d4eab
lamons <lamons.n@gmail.com>
2020-01-05 15:45:49 +0800

    rebuilt

--- article_retrieval.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_GET["article"])) {
-    $article = $_GET["article"];
-    $fileName = 'articles/'.$article.'.php';
-    $myfile = fopen($fileName, "r") or die("Unable to open file!");
-    echo fread($myfile,filesize($fileName));
-    fclose($myfile);
-}
-?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,50 @@
+function hidelv2(){
+	$(".lv3").hide();
+	$(".lv2").hide();
+}
+
+function hidelv3(){
+	$(".lv3").hide();
+}
+
 $(document).ready(function(){
 	$(".publication-toggle").click(function(){
-		$(".menu-lv2").hide();
-		$(".lv3").hide();
-		$(".logo3d-container").css({"position": "absolute", "z-index": "-100"});
-		$(".logo3d-container").offset({ top: l3y, left: l3x });
-		$(".pub-content").offset({ top: pty});
+		hidelv2();
 		$(".publication").show();
-		$(".music-toggle").css("text-decoration", "none");
-		$(".about-toggle").css("text-decoration", "none");
-		$(".publication-index").css("text-decoration", "none");
-		$(this).css("text-decoration", "line-through");
 	});
-});
-$(document).ready(function(){
-	$(".music-toggle").click(function(){
-		$(".menu-lv2").hide();
-		$(".lv3").hide();
-		$(".logo3d-container").css({"position": "absolute", "z-index": "-100"});
-		$(".logo3d-container").offset({ top: l3y, left: l3x });
-		$(".music-content").offset({ top: mty});
-		$(".publication-index").css("text-decoration", "none");
-		$(".music").show();
-		$(".about-toggle").css("text-decoration", "none");
-		$(".publication-toggle").css("text-decoration", "none");
-		$(".publication-index").css("text-decoration", "none");
-		$(this).css("text-decoration", "line-through");
-	});
-});
-$(document).ready(function(){
 	$(".about-toggle").click(function(){
-		$(".menu-lv2").hide();
-		$(".lv3").hide();
-		$(".logo3d-container").css({"position": "absolute", "z-index": "-100"});
-		$(".logo3d-container").offset({ top: l3y, left: l3x });
-		$(".about-content").offset({ top: aty});
-		$(".publication-index").css("text-decoration", "none");
+		hidelv2();
 		$(".about").show();
-		$(".publication-toggle").css("text-decoration", "none");
-		$(".music-toggle").css("text-decoration", "none");
-		$(".publication-index").css("text-decoration", "none");
-		$(this).css("text-decoration", "line-through");
 	});
 });
 
-$(".joyride").click(function(){
-	console.log("sss");
-	$.ajax({
-		url: 'index_zh.php',
-		data: {"article": alvalee},
-		type: 'GET',
-		success: function(data)
-		{
-			$(".entry").html(data);
-		}
+$(document).ready(function(){
+	$(".pub-entry-toggle").click(function(){
+		hidelv3();
 	});
-
-	$(".lv3").hide();
-	$(".joyride-entry").show("slide", { direction: "left" }, 300);
-	$(".publication-index").css("text-decoration", "none");
-	$(this).css("text-decoration", "line-through");
+	$("#cx1").click(function(){
+		$(".entry").show();
+		$("#entry-cx1").show();
+	});
 });
+
+// $(".joyride").click(function(){
+	// console.log("sss");
+	// $.ajax({
+		// url: 'index_zh.php',
+		// data: {"article": alvalee},
+		// type: 'GET',
+		// success: function(data)
+		// {
+			// $(".entry").html(data);
+		// }
+	// });
+
+	// $(".lv3").hide();
+	// $(".joyride-entry").show("slide", { direction: "left" }, 300);
+	// $(".publication-index").css("text-decoration", "none");
+	// $(this).css("text-decoration", "line-through");
+// });
 
 
 //$(document).ready(function(){
