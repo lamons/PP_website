77ab827045747b427cf57a527f8b04ebf1f2349d
lamons <lamons.n@gmail.com>
2020-01-06 18:32:41 +0800

    add anchors

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,10 @@ function hidelv3(){
 	$(".lv3").hide();
 }
 
+function getAnchor() {
+    return (document.URL.split('#').length > 1) ? document.URL.split('#')[1] : null;
+}
+
 $(document).ready(function(){
 	$(".publication-toggle").click(function(){
 		hidelv2();
@@ -26,6 +30,29 @@ $(document).ready(function(){
 		$(".entry").show();
 		$("#entry-cx1").show();
 	});
+});
+
+$(document).ready(function(){
+	var pagehash = getAnchor();
+	if (pagehash == 'entry-cx1') {
+		$(".publication").show();
+		$(".entry").show();
+		$("#entry-cx1").show();
+	}
+});
+
+$(document).ready(function(){
+	var pagehash = getAnchor();
+	if (pagehash == 'about') {
+		$(".about").show();
+	}
+});
+
+$(document).ready(function(){
+	var pagehash = getAnchor();
+	if (pagehash == 'publication') {
+		$(".publication").show();
+	}
 });
 
 // $(".joyride").click(function(){
